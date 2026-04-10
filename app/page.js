2d69a7337377a8f8ed1aaee5ea91ab06f4d65a85
Themugo <mugo.james27@gmail.com>
2026-04-10 18:48:04 +0300

    Delete app directory

--- app/page.js
+++
@@ -1,7 +0,0 @@
-"use client";
-import dynamic from "next/dynamic";
-const LiveMap = dynamic(()=>import("../components/LiveMap"),{ssr:false});
-
-export default function Page(){
- return <div style={{padding:20}}><h1>SimTrace</h1><LiveMap/></div>
-}
